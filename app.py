b5e631897132e11c84531c560bec0af737bb6074
raghaviquber <raghavimanyam@gmail.com>
2026-03-24 16:02:38 +0530

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 # -----------------------------
 # MODEL
